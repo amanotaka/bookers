3568ccab7a9ceaf4647c48f6fddff12c684c5ef9
amanotaka <amanokuma@gmail.com>
2023-08-31 12:11:44 +0000

    in the middle

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,25 @@ class BooksController < ApplicationController
     @book = Book.new
   end
   
+  def create
+    book = Book.new(book_params)   # 1.$2. データを受け取り新規登録するためのインスタンス作成
+    book.save                      #3.     データをデータベースに保存するためのsaveメソッド実行
+    redirect_to '/top'             #4.     トップ画面へリダイレクト
+  end
+  
   def index
+    @books = Book.all
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  #ストロングパラメータ
+  def list_params
+    params.repuire(:book).permit(:book, :body)
   end
 end
