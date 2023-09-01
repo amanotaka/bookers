28bb7e980143876df37f0fdc13e9d83d34494260
amanotaka <amanokuma@gmail.com>
2023-09-01 12:08:01 +0000

    in the middle

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get 'books' => 'books#index'
   root 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # .../lists/1 や .../lists/3に該当する
+  get 'lists/:id' => 'lists#show'
 end
